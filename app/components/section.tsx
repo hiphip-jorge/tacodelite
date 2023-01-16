980929c3371beca8d105d9c67d5ee19bf4fc46b3
Jorge Perez <jorgeperez@MacBook-Air.local>
2023-01-16 17:38:31 -0600

    feature: completed responsive for main sections. Footer needs work. Todo: add doordash and ubereats links in lg >

--- app/components/section.tsx
+++ app/components/section.tsx
@@ -8,6 +8,7 @@ type Props = {
   children: React.ReactNode;
   height?: string;
   width?: string;
+  alt?: boolean;
 };
 
 const Section = (props: Props) => {
@@ -15,13 +16,20 @@ const Section = (props: Props) => {
     ? props.sectionClass
     : "flex flex-col justify-around py-10";
   sectionClass += props.height ? ` ${props.height}` : "";
-  sectionClass += props.width ? ` ${props.width}` : "";
+  sectionClass += props.width
+    ? ` ${props.width}`
+    : " xl:max-w-[1200px] xl:m-auto";
+  sectionClass += props.alt ? " bg-dark" : "";
 
   return (
     <section className={sectionClass}>
       <h1
-        className={`mx-auto w-fit font-primary-outline text-6xl text-green-primary md:text-7xl ${
+        className={`mx-auto w-fit text-6xl text-green-primary md:text-7xl ${
           props.hClass ? props.hClass : ""
+        } ${
+          props.alt
+            ? " font-primary-solid text-green-light"
+            : "font-primary-outline"
         }`}
       >
         {props.header}
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -49,7 +49,7 @@ function Index() {
 
   const categories = useLoaderData<category[]>();
   const categoryRefs = categories.map(() => {
-    return useInView({ threshold: 1, rootMargin: "0px 0px -295px 0px" });
+    return useInView({ threshold: 1, rootMargin: "0px 0px -300px 0px" });
   });
 
   const handleToggle = (e: React.SyntheticEvent) => {
@@ -98,9 +98,9 @@ function Index() {
             <img className="z-10" src={catering} alt="plate 1 image" />
           </picture>
           <div className="px-12 md:px-28">
-            <h2 className="hero-h2 text-green-dark">Fresh Everyday.</h2>
-            <h2 className="hero-h2 text-end text-green-primary">
-              Real Ingredients.
+            <h2 className="hero-h2 text-green-primary">Real Ingredients.</h2>
+            <h2 className="hero-h2 text-end text-green-dark">
+              Fresh Everyday.
             </h2>
           </div>
           <div className="mx-auto flex w-fit gap-4">
@@ -145,51 +145,147 @@ function Index() {
         </aside>
         {/* About Us Section */}
         <Section header="About Us">
-          <div className="px-16 pl-[calc(4rem-1rem)]">
-            <figure className="flex justify-center">
+          <div className="flex flex-col items-center gap-4 px-16 pl-[calc(4rem-1rem)] md:px-12 xl:my-10 xl:flex-row xl:gap-0">
+            <figure className="flex justify-center md:px-8 xl:px-0">
               <img
                 src={td_building}
                 className="my-4 rounded-3xl shadow-lg md:w-3/4"
                 alt=""
               />
             </figure>
-            <p className="font-secondary-secular text-lg text-dark md:px-20 md:text-2xl">
+            <p className="h-fit font-secondary-secular text-lg text-green-dark md:px-20 md:text-2xl xl:px-0">
               {aboutUs_p}
             </p>
           </div>
         </Section>
         {/* Menu Section  */}
         <Section header="Menu">
-          <div>
-            {categories.map((category, idx) => (
-              <div id={category.name.toLowerCase()} key={category.name}>
-                <h1
-                  id={category.name}
-                  ref={categoryRefs[idx].ref}
-                  className={`menuSectionHeader underline-effect ${
-                    categoryRefs[idx].inView && "in--view"
-                  }`}
-                >
-                  {category.name}
-                </h1>
-                <div className="flex flex-wrap justify-start gap-4 px-12 md:px-16">
-                  {category.foodItems.map((item, idx) => (
-                    <Card
-                      id={item.name.replaceAll(" ", "-")}
+          <div className="lg:my-8 lg:flex lg:gap-2 xl:my-16">
+            {/* Side nav */}
+            <aside className="sticky top-16 left-10 hidden h-fit lg:inline lg:w-1/4">
+              <ul>
+                {categories.map((category, idx) => {
+                  return (
+                    <li
                       key={idx}
-                      item={item}
-                    />
-                  ))}
+                      className="flex justify-center p-2 text-green-light"
+                    >
+                      <a
+                        className={`h-full w-full font-primary-solid text-3xl duration-500 ease-in-out xl:text-left ${
+                          categoryRefs[idx].inView && "text-green-dark"
+                        }`}
+                        href={"#" + category.name}
+                      >
+                        {category.name}
+                      </a>
+                    </li>
+                  );
+                })}
+              </ul>
+            </aside>
+            {/* Menu cards  */}
+            <div className="px-2 md:px-6 lg:w-3/4 lg:px-0">
+              {categories.map((category, idx) => (
+                <div
+                  id={category.name.toLowerCase()}
+                  key={category.name}
+                  className="xl:mb-24"
+                >
+                  <h1
+                    id={category.name}
+                    ref={categoryRefs[idx].ref}
+                    className={`menuSectionHeader underline-effect ${
+                      categoryRefs[idx].inView && "in--view"
+                    }`}
+                  >
+                    {category.name}
+                  </h1>
+                  <div className="flex flex-wrap justify-start gap-4 px-12 md:px-16 lg:px-4 xl:gap-8 xl:px-8">
+                    {category.foodItems.map((item, idx) => (
+                      <Card
+                        id={item.name.replaceAll(" ", "-")}
+                        key={idx}
+                        item={item}
+                      />
+                    ))}
+                  </div>
                 </div>
-              </div>
-            ))}
+              ))}
+            </div>
           </div>
         </Section>
       </main>
-      <footer>
-        <Section header="footer">
-          <div className="h-[1000px]"></div>
-        </Section>
+      <footer className="flex flex-col justify-center gap-6 bg-green-dark p-10">
+        <div className="w-max-[1200px] m-auto flex w-1/2 flex-col gap-4 lg:flex-row lg:flex-wrap md:gap-8">
+          <div className="w-fit">
+            <h1 className="font-primary-solid text-3xl text-light">Location</h1>
+            <ul className="flex flex-col font-secondary-secular">
+              <li>
+                <a
+                  href="https://www.google.com/maps/dir/''/Taco+Delite,+West+15th+Street,+Plano,+TX/@33.0210863,-96.7853179,13z/data=!3m2!4b1!5s0x864c22c760f8bec1:0x153e7787779c5cc6!4m8!4m7!1m0!1m5!1m1!1s0x864c22779cbdf961:0x122a03406b2f3e01!2m2!1d-96.7503414!2d33.0210227"
+                  className="text-green-light duration-300 hover:text-white"
+                >
+                  2957 W 15th Street, Plano, TX, 75075
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div className="w-fit">
+            <h1 className="font-primary-solid text-3xl text-light">
+              Contact Us
+            </h1>
+            <ul className="flex flex-col font-secondary-secular">
+              <li>
+                <a
+                  href="tel: 972-964-5419"
+                  className="text-green-light duration-300 hover:text-white"
+                >
+                  972-964-5419
+                </a>
+              </li>
+              <li>
+                <a
+                  href="email: taco delite"
+                  className="text-green-light duration-300 hover:text-white"
+                >
+                  tacodelitewestplano@gmail.com
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div className="w-fit">
+            <h1 className="font-primary-solid text-3xl text-light">
+              Follow Us
+            </h1>
+            <ul className="flex flex-col font-secondary-secular">
+              <li>
+                <a
+                  href="https://www.facebook.com/TacoDeliteWestPlano"
+                  className="text-green-light duration-300 hover:text-white"
+                >
+                  Facebook @ Taco Delite West Plano
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div className="w-fit">
+            <h1 className="font-primary-solid text-3xl text-light">
+              Store Hours
+            </h1>
+            <ul className="flex flex-col font-secondary-secular">
+              <li className="text-green-light">Mon-Sat: 7am-9pm</li>
+              <li className="text-green-light">Sundays: closed</li>
+            </ul>
+          </div>
+        </div>
+        <div className="flex w-full items-center justify-center gap-2">
+          <figure className="flex h-8 w-8 items-end justify-start">
+            <img src={taco_delite} alt="taco delite logo" className="w-8" />
+          </figure>
+          <p className="font-secondary-secular text-white ">
+            CJN Inc. All right reserved.
+          </p>
+        </div>
       </footer>
       <Modal
         contentList={currentContent}
